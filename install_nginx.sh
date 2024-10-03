d387bc079737f43e20febc34de7a3a53f950c8c7
Jaaystones <Joelpaul345@gmail.com>
2024-10-03 17:11:53 +0100

    :wrench: Pipeline testing

--- install_nginx.sh
+++ install_nginx.sh
@@ -8,6 +8,3 @@ sudo apt install nginx -y
 
 # Restart Nginx
 sudo systemctl restart nginx
-
-# Output the public IP
-#echo "Public IP: $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
