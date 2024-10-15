3193cc7d1dc06371143848fbdb95f87d74a08b4e
Jaaystones <Joelpaul345@gmail.com>
2024-10-15 10:08:54 +0100

    :wrench: Testing webhook

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt update -y
+# Fix any interrupted package installations
+sudo dpkg --configure -a
 
 # Install Nginx
-sudo apt install nginx -y
+sudo apt-get update
+sudo apt-get install nginx -y
 
-# Restart Nginx
+# Restart Nginx service
 sudo systemctl restart nginx
+sudo systemctl enable nginx
+
